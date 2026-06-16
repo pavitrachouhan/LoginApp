3a280e8bdbe8f1bbb94f1d63b1fe602ecb6a2996
Pavitra Chouhan <your-email@example.com>
2026-06-16 22:39:58 +0530

    Add PostgreSQL debug

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,4 +12,13 @@ const pool = new Pool({
   },
 });
 
+pool.connect()
+  .then(() => {
+    console.log("✅ PostgreSQL Connected");
+  })
+  .catch((err) => {
+    console.error("❌ PostgreSQL Connection Error:");
+    console.error(err);
+  });
+
 module.exports = pool;
